e228cb4c92f365cd3e37c69f99dcbbee8b9251e8
Kai En Yong <s10173513@connect.np.edu.sg>
2022-03-17 21:03:00 +0800

    Added Download Image Feature

--- client.py
+++ client.py
@@ -4,17 +4,39 @@ import threading
 
 nickname = input("Please enter your nickname: ")
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 33333))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IP, SOCK_STREAM = TCP
+client.connect(('127.0.0.1', 33333)) #localhost
+
+BUFFER_SIZE = 1024
 
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(BUFFER_SIZE).decode('ascii')
+
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
+
+            if message == 'SENDINGCHUNKS':
+                number_chunks = int(client.recv(1024).decode('ascii')) #Convert the number of chunks to integer from String
+                file = open('downloaded/downloaded_image.png', 'wb')
+
+                # This is because TCP/IP transfers in streams
+
+                while number_chunks > 0:
+                    image_chunk = client.recv(BUFFER_SIZE)
+
+                    file.write(image_chunk)
+                    number_chunks -= 1
+
+                print("Image Downloaded Successfully")
+
+                file.close()
+
             else:
                 print(message)
+
+
         except:
             print("An error has occurred!")
             client.close()
@@ -22,8 +44,13 @@ def receive():
 
 def write():
     while True:
-        message = f'{nickname}: {input("")}' #Constantly waiting for new messages
+        message = f'{nickname} > {input("")}' #Constantly waiting for new messages
         client.send(message.encode('ascii'))
+
+
+
+
+
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import threading
 import socket
+import os
 
 host = '127.0.0.1' #Running on localhost (no server)
 
@@ -8,6 +9,8 @@ port = 33333
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen() #Start listening for incoming ports
+
+BUFFER_SIZE = 1024 #Buffer size of 1024 bytes
 
 clients = [] #List of clients
 nicknames = [] #List of nicknames
@@ -20,7 +23,44 @@ def handle(client):
     while True:
         try:
             message = client.recv(1024) #Try to receive a message from client (up to 1024 bytes)
-            broadcast(message) #If received, then broadcast this message to all other clients
+            temp = message.decode('ascii')
+            if "LIST images" in temp:
+                arr = os.listdir('./images')
+                for i in arr:
+                    client.send(i.encode('ascii'))
+
+
+            elif "DOWNLOAD" in temp:
+                split_arr = temp.split(' ') #Split up the message with space delimiter
+
+                file_name = './images/' + split_arr[3] #Concatenate file name into string
+                file = open(file_name, 'rb')  # rb is read-binary
+
+                chunks = 0
+                l = file.read(BUFFER_SIZE)
+                while l:
+                    l = file.read(BUFFER_SIZE)
+                    chunks += 1 #To get the number of chunks required to send the image file
+                file.close()
+
+                message = 'SENDINGCHUNKS'.encode('ascii')
+                client.send(message)
+
+                message_chunks = str(chunks).encode('ascii')
+                client.send(message_chunks) #Send the number of chunks to the client
+
+                file = open(file_name, 'rb')
+                image_data = file.read(BUFFER_SIZE)
+
+                while image_data:
+                    client.send(image_data)
+                    image_data = file.read(BUFFER_SIZE)
+
+                file.close()
+
+
+            else:
+                broadcast(message) #If received, then broadcast this message to all other clients
 
         except:
             index = clients.index(client) #Obtain the index of the client
@@ -49,6 +89,33 @@ def receive():
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
+def write():
+    while True:
+        message = f'{nickname} > {input("")}' #Constantly waiting for new messages
+        server.send(message.encode('ascii'))
+
+# def receive_message():
+#     while True:
+#         try:
+#             message = server.recv(BUFFER_SIZE).decode('ascii')
+#             if message == 'NICK':
+#                 admin.send(nickname.encode('ascii'))
+#             print(message)
+#
+#         except:
+#             print("An error has occurred!")
+#             server.close()
+#             break
+
+print("Server is listening...")
+# receive_thread = threading.Thread(target=receive_message)
+# receive_thread.start()
+#
+# write_thread = threading.Thread(target=write)
+# write_thread.start()
+
 receive()
+
+
 
 
