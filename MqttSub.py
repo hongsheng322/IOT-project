46a1d075e93bb8de4b53aa54fa52ed413ecb29ce
Duck Horse <hongsheng322@gmail.com>
2022-03-26 14:16:32 +0800

    MQTT publisher and Subscriber
    subscriber will be the living room pi

---
+++ MqttSub.py
@@ -0,0 +1,34 @@
+
+import paho.mqtt.client as mqtt_client
+import time
+
+broker = 'test.mosquitto.org'
+port = 1883
+topic = "python/mqtt"
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client("Living_rm_PI")
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+if __name__ == '__main__':
+    run()
